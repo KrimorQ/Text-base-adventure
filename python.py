9c4ba91b52515b2df7e1cc4a047640166421b78b
Kristoffer <151470247+Qfiss@users.noreply.github.com>
2024-02-01 17:54:50 +0100

    Update python.py
    currently not working

--- python.py
+++ python.py
@@ -1,6 +1,14 @@
-print ("Press Y to enter or Q to quit") #start screen
+print("Press Y to enter or Q to quit") #start screen
 
+user = input("Your choice: ").upper() #what to enter to play (Y,Q)
 
-user = input("Your choice: ") #what to enter to play (Y,Q)
+if user == "Y":
+    print("Starting the game...")
+
+elif user == "Q":
+    print("Closing the game")
+
+else:
+    print("Please enter valid command (Y,Q)")
 
 
