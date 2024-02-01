72c1cb12e723e09556c501b5db628a78a78fea9b
Kristoffer <151470247+Qfiss@users.noreply.github.com>
2024-02-01 18:13:08 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,12 +1,17 @@
 print("Welcome to an adventure, press Y to play or Q to Exit")
 
-user = input("Your choice: ").upper() #what to enter to play (Y,Q)
 
-if user == "Y":
-    print("Starting the game...")
+while True:
+    user = input("Your choice: ").upper() #what to enter to play (Y,Q)
 
-elif user == "Q":
-    print("Closing the game")
 
-else:
-    print("Please enter valid command (Y,Q)")
+    if user == "Y":
+        print("Starting the game...")
+        break #Exit loop
+
+    elif user == "Q":
+        print("Closing the game")
+        break #Exit loop
+
+    else:
+        print("Please enter valid command (Y,Q)")
