dbd50e0b256a74b82c903a8d9ab54ec10defb348
Kristoffer <151470247+Qfiss@users.noreply.github.com>
2024-02-01 17:47:25 +0100

    Update python.py

---
+++ python.py
@@ -0,0 +1,9 @@
+print ("Press Y to enter or Q to quit") #start screen
+
+print ("Your choice: ") #what to enter to play (Y,Q)
+
+while True:
+    if input() == "Y"
+    print("Welcome to my adventure")
+
+
