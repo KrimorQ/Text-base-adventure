f87b11c015e995ba00b506568683b7d2572bf2b5
Kristoffer <151470247+Qfiss@users.noreply.github.com>
2024-02-03 00:03:36 +0100

    Update python.py
    character name and class option (not finished yet)

--- python.py
+++ python.py
@@ -1,5 +1,7 @@
 print("Welcome to an adventure, press Y to play or Q to Exit")
 
+player_classes = ["Warrior", "Mage", "Rouge"]
+class_index = 0
 
 while True:
     user = input("Your choice: ").upper() #what to enter to play (Y,Q)
@@ -15,3 +17,8 @@ while True:
 
     else:
         print("Please enter valid command (Y,Q)")
+
+character_name = input("What is your name? ")
+print (f"{character_name} What class would you like to play (press N) to show next class available")
+
+
